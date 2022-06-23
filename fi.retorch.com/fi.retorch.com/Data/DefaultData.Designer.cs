59dad16f21d4ae668753247b665c1885c95ee8a8
Jason Beck <jason.beck@joyninsurance.com>
2022-06-23 11:10:19 -0400

    Upgrade to .NET 4.8 and newer Nuget packages

--- fi.retorch.com/fi.retorch.com/Data/DefaultData.Designer.cs
+++ fi.retorch.com/fi.retorch.com/Data/DefaultData.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\BitBucket\fi.retorch.com\fi.retorch.com\fi.retorch.com\Data\DefaultData.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\online repos\fi-retorch\fi.retorch.com\fi.retorch.com\Data\DefaultData.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
